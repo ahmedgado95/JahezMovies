3acec8f7c5ccd79d85164e6a84a8fc166942855f
AhmedGado <ahmedgado2001@gmail.com>
2025-08-12 13:45:55 +0300

    Initial Commit

---
+++ JahezMovies/JahezMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JahezMoviesApp.swift
+//  JahezMovies
+//
+//  Created by ahmed gado on 12/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct JahezMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
